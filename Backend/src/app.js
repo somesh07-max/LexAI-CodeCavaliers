6d11067c1dbcdaff565b25ca6070f63c6857f5cd
somesh dewangan <someshdew30062020@gmail.com>
2026-08-28 15:45:34 +0530

    added cors in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
- 
+const cors = require("cors");
  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
  
- 
+ app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    })
+);
  
  
 const authRoutes = require("./routes/Authentication.routes.js");
